f5ec1c2fbdf6124c6400b3d571d88b20f5348c31
ahmadfahrezi074-pixel <ahmadfahrezi074@gmail.com>
2026-05-22 23:22:48 +0700

    update tombol layanan

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,9 +46,15 @@ export default function Home() {
   </button>
 </a>
 
-            <button className="bg-white/10 backdrop-blur-md border border-white/20 hover:bg-white/20 px-8 py-4 rounded-2xl text-lg font-semibold transition duration-300 hover:scale-105">
-              Lihat Layanan
-            </button>
+            <a href="/dashboard">
+
+  <button className="bg-white/10 backdrop-blur-md border border-white/20 hover:bg-white/20 px-8 py-4 rounded-2xl text-lg font-semibold transition duration-300 hover:scale-105">
+
+    Lihat Layanan
+
+  </button>
+
+</a>
 
           </div>
 
